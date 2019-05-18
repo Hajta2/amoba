03b65eb5c37bebc1cc038c4424bd46ac65585ead
Hajta2 <50477868+Hajta2@users.noreply.github.com>
2019-05-18 15:45:57 +0200

    Add files via upload

---
+++ Buttons.hpp
@@ -0,0 +1,26 @@
+#ifndef BUTTONS_HPP
+#define BUTTONS_HPP
+
+#include "widgets.hpp"
+#include <functional>
+
+
+class Buttons: public Widget
+{
+public:
+  Buttons(Window *w, int x, int y, int sx, int sy);
+  void draw() override;
+  void handle(genv::event ev) override;
+  virtual void action() = 0;
+};
+
+class FunctorButton: public Buttons{
+std::function<void()> fv;
+public:
+    FunctorButton(Window *w,int x,int y,int sx,int sy, std::function<void()> _fv);
+    void action() override;
+};
+
+
+
+#endif // BUTTONS_HPP
