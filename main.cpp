3c4c0be661aa3e05ff64e54aae33ed656add4793
Hajta2 <50477868+Hajta2@users.noreply.github.com>
2019-05-22 11:19:20 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "graphics.hpp"
+#include "widgets.hpp"
+#include "window.hpp"
+#include "Game.hpp"
+#include <vector>
+#include <iostream>
+#include "start.hpp"
+#include "Board.hpp"
+#include "GameLogic.hpp"
+using namespace std;
+using namespace genv;
+
+
+
+int main() {
+  start *amoba = new start();
+  amoba->Start();
+  delete amoba;
+  return 0;
+}
